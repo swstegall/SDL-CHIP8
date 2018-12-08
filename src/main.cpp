4de1086fe086254df7d495534318f2bdaf4e611d
Samuel Stegall <swstegall@tuxtop.Home>
2018-12-08 03:32:22 -0600

    Fixed rendering issues.

--- src/main.cpp
+++ src/main.cpp
@@ -16,16 +16,10 @@
 
 using namespace std;
 
-const int FRAMES_PER_SECOND = 60;
-
 int main(int argc, char* args[])
 {
   //Initialize game object
   Game play;
-
-  //Timing
-  time_t timer;
-  int total_frames = 0;
 
 	//Set up render system and register input callbacks
 	play.setup_graphics();
@@ -38,36 +32,16 @@ int main(int argc, char* args[])
 		//Emulate Cycle
 		play.emulate_cycle();
 
-		//DEBUG
-		//play.printGFX();
-		//cout << endl;
-
 		//If the draw flag is set, update the screen
 		if(play.drawFlag)
     {
       play.draw_graphics();
       play.drawFlag = false;
+      SDL_Delay(FRAME_DELAY);
     }
 
 		//Store key press state (Press and Release)
 		play.set_keys();
-
-    if(total_frames % 2 == 0)
-    {
-      SDL_SetRenderDrawColor(play.gRenderer, 0x00, 0x00, 0x00, 0xFF);
-      SDL_RenderClear(play.gRenderer);
-      SDL_SetRenderDrawColor(play.gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
-      SDL_RenderPresent(play.gRenderer);
-    }
-
-    //Cap the frame rate to a defined fps value
-     if(time(&timer) < 1000 / FRAMES_PER_SECOND)
-     {
-         //Sleep the remaining frame time
-         SDL_Delay((1000 / FRAMES_PER_SECOND) - time(&timer));
-     }
-
-     ++total_frames;
 	}
 
 	//Free resources and close SDL
